bc70c248cef658666cd031468eef6b72a0e044f4
sjaces <tellme@angelcachon.es>
2019-01-28 23:15:18 +0100

    Making things neat, mouse and touch events work fine and only one piece can be selected

--- js/sketch.js
+++ js/sketch.js
@@ -1,36 +1,149 @@
+var myImage;
+const piecesSize = 100;
+var half = piecesSize/2;
+var thereIsASelection=true;
+
+function preload()
+{
+  // load images
+  rojo = loadImage("./imagenes/rojo.png");
+  rojo.x=200, rojo.y=100, rojo.nombre="rojo"; 
+  azul = loadImage("./imagenes/azul.png");
+  azul.x=10+piecesSize, azul.y=150+piecesSize, azul.nombre="azul";
+  verde = loadImage("./imagenes/verde.png");
+  verde.x=0, verde.y=50, verde.nombre="verde";
+  amarillo = loadImage("./imagenes/amarillo.png");
+  amarillo.x=20, amarillo.y=0, amarillo.nombre="amarillo";
+  rojo.activo=azul.activo=verde.activo=amarillo.activo=false;
+
+  piezas = [rojo, azul, verde, amarillo];
+}
+
+
 function setup() {
   // put setup code here
-  createCanvas(720, 400);
-  background(240);
-  var myX= mouseX;
-  var myY = mouseY;
-  var aX=aY=bX=bY=250;
-  fill(120,20,20);
-  a = ellipse(aX,aY,50,50);
   
-  fill(20,20,120);
-  b = ellipse(bX,bY,90,90);
+  anchoPantalla = window.innerWidth;
+  altoPantalla = window.innerHeight;
+  
+  createCanvas(anchoPantalla, altoPantalla);
+  background(240);
+  
+
+  // image(azul, 300+50, 200+50, piecesSize, piecesSize);
+  
+  // image(rojo, 300, 200, piecesSize,piecesSize);
+  // loadImage('./imagenes/rojo.png', img => {image(myImage, 100, 100, 300 , 300)});
 }
 
 function draw() {
   // put drawing code here
-  var myX= mouseX;
-  var myY = mouseY;
+  
   background(240);
-  fill(120,20,20);
-  if (mouseIsPressed) {
-    aX = mouseX;
-  aY = mouseY;
-  ellipse(aX,aY,50,50);
-  } else {
+
+  
+
+  image(rojo, rojo.x, rojo.y, piecesSize, piecesSize);
+  image(azul, azul.x, azul.y, piecesSize, piecesSize);
+  image(verde, verde.x, verde.y, piecesSize, piecesSize);
+  image(amarillo, amarillo.x, amarillo.y, piecesSize, piecesSize);
+  
+  if(mouseIsPressed){
+
+    mover();
     
-    bX = mouseX;
-    bY = mouseY;
+  } else {
+    rojo.activo=azul.activo=verde.activo=amarillo.activo=false;
+    thereIsASelection = false;
   }
 
 
 
+  
+}
 
-  fill(20,20,120);
-  ellipse(bX,bY,90,90)
+function amIOnTop(pieza){
+    // I see if my mouse is on top of the piece
+    if(mouseX<pieza.x+piecesSize && mouseX>pieza.x 
+    && mouseY<pieza.y+piecesSize && mouseY>pieza.y
+      ) {
+        // console.log(`Estoy encima de ${pieza.nombre}`);
+        return true;
+      } else {
+        return false;
+      }
+      
+    }
+    
+    function anythingClicked(pieza) {
+      var clicked = false;
+      // if({piezas.activo} && pieza.activo == true){
+        //   return true;
+        // }
+
+
+        // I'm looking if there is a selection
+      if(thereIsASelection){
+
+        if(pieza.activo){
+          return true;
+        }else{
+          return false;
+        }
+
+      }
+
+      pieza.activo = true;
+      thereIsASelection = true;
+      return true;
+
+
+
+        
+}
+
+function mover() {
+
+  if (amIOnTop(azul) && anythingClicked(azul)) { 
+    azul.x = mouseX-half;
+    azul.y = mouseY-half;
+    return;
+  } else if (amIOnTop(rojo) && anythingClicked(rojo)) {
+    rojo.x = mouseX-half;
+    rojo.y = mouseY-half;
+    return;
+  } else if (amIOnTop(verde)  && anythingClicked(verde)) {
+    verde.x = mouseX-half;
+    verde.y = mouseY-half;
+    return;
+  } else if (amIOnTop(amarillo) && anythingClicked(amarillo)) {
+    amarillo.x = mouseX-half;
+    amarillo.y = mouseY-half;
+    return;
+  } 
+  
+}
+
+function touchMoved() {
+
+  mover();
+  // if (amIOnTop(azul)) { 
+  //   azul.x = mouseX-half;
+  //   azul.y = mouseY-half;
+  //   return;
+  // } else if (amIOnTop(rojo)) {
+  //   rojo.x = mouseX-half;
+  //   rojo.y = mouseY-half;
+  //   return;
+  // } else if (amIOnTop(verde)) {
+  //   verde.x = mouseX-half;
+  //   verde.y = mouseY-half;
+  //   return;
+  // } else if (amIOnTop(amarillo)) {
+  //   amarillo.x = mouseX-half;
+  //   amarillo.y = mouseY-half;
+  //   return;
+  // } 
+
+  
 }
